e39ebd4d9e1d295c9d4637dcf019093c0134df28
Caio Agiani <caio.pimenta@infracommerce.com.br>
2021-06-26 19:14:23 -0300

    feat(project): config typeorm with mongodb and graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { EmailsModule } from './emails/emails.module';
 
 @Module({
-  imports: [EmailsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb://localhost:27017/graphql',
+      synchronize: true,
+      autoLoadEntities: true,
+      useUnifiedTopology: true,
+    }),
+    EmailsModule,
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
+    }),
+  ],
 })
 export class AppModule {}
